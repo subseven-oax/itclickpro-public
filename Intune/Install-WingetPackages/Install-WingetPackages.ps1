95963b07e8c903b669f0834e0eec243f4a7c1879
Victor Valentin <victor@itclickpro.com>
2024-06-24 17:13:09 +0100

    Updated winget detection

--- Intune/Install-WingetPackages/Install-WingetPackages.ps1
+++ Intune/Install-WingetPackages/Install-WingetPackages.ps1
@@ -3,14 +3,13 @@
 
 # Define list of Winget ID packages to install, on a device with winget install use the 'winget search <AppName>' command to find the package IDs
 $WingetPackages = @("9NZVDKPMR9RD","7zip.7zip","XP9KHM4BK9FZ7Q")  # This example installs Firefox, 7-Zip and Visual Studio Code
-$MyApp = Get-AppxPackage –AllUsers | Select-Object Name, PackageFullName | Where-Object {$_.Name -eq "Microsoft.Winget.Source"}
 
-#Logic To Install Winget or Skip
-if ($MyApp.Name -eq "Microsoft.Winget.Source"){
-	Write-Output "Winget is already installed on this computer."
-}
-else{
-	$progressPreference = 'silentlyContinue'
+# Check if Winget is installed
+$wingetInstalled = Get-Command winget -ErrorAction SilentlyContinue
+
+if (-not $wingetInstalled) {
+    # Get the latest download URL for Winget
+    $progressPreference = 'silentlyContinue'
 	Write-Information "Downloading WinGet, its dependencies, and install them"
 	Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 	Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
@@ -18,6 +17,9 @@ else{
 	Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 	Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
 	Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+    Write-Host "Winget has been installed successfully!"
+} else {
+    Write-Host "Winget is already installed."
 }
 
 # Install all Winget packages
