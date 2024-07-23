20abbaa8eac7e3a76c77e5004507e6736156ae94
Victor Valentin <victor@itclickpro.com>
2024-07-23 16:33:59 +0100

    Removed email address

--- Intune/Edge-And-Chrome-History/Remediate_Edge_And_Chrome_Browsing_History.ps1
+++ Intune/Edge-And-Chrome-History/Remediate_Edge_And_Chrome_Browsing_History.ps1
@@ -34,13 +34,13 @@ function Get-ChromeHistory {
     }
 }
 
-$adminfolder = "c:\Admin"
+$adminfolder = "c:\Admin"  # CHANGE FOLDER
 New-Item $adminfolder -ItemType Directory -Force
 
 $chromeHistory = Get-ChromeHistory
-$chromeHistory | Export-Csv -Path "C:\Admin\chrome_history.csv" -Force -NoTypeInformation
+$chromeHistory | Export-Csv -Path "C:\Admin\chrome_history.csv" -Force -NoTypeInformation  # CHANGE PATH
 $edgeHistory = Get-EdgeHistory
-$edgeHistory | Export-Csv -Path "C:\Admin\edge_history.csv" -Force -NoTypeInformation
+$edgeHistory | Export-Csv -Path "C:\Admin\edge_history.csv" -Force -NoTypeInformation  # CHANGE PATH
 
 # Create a new Outlook application object
 $outlookProcess = Get-Process -Name outlook -ErrorAction SilentlyContinue
@@ -50,9 +50,9 @@ $mail = $outlook.CreateItem(0)
 # Set the email properties
 $mail.Subject = "Browsing History for Edge and Chrome"
 $mail.Body = "Please find attached my browsing history"
-$mail.To = "support@itclickpro.com"
+$mail.To = "mailbox@domain.com"   # CHANGE EMAIL ADDRESS
 # Add multiple attachments
-$attachments = @("C:\\Admin\\chrome_history.csv", "C:\\Admin\\edge_history.csv")
+$attachments = @("C:\\Admin\\chrome_history.csv", "C:\\Admin\\edge_history.csv")   # CHANGE PATHS
 foreach ($attachment in $attachments) {
     $mail.Attachments.Add($attachment)
 }
