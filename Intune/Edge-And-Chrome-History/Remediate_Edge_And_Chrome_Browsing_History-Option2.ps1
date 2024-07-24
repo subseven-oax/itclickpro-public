02c9851670eb64da69a26ca75ec92ea6d6965ed6
Victor Valentin <victor@itclickpro.com>
2024-07-24 23:58:08 +0100

    Major changes on Remediate option 2

--- Intune/Edge-And-Chrome-History/Remediate_Edge_And_Chrome_Browsing_History-Option2.ps1
+++ Intune/Edge-And-Chrome-History/Remediate_Edge_And_Chrome_Browsing_History-Option2.ps1
@@ -2,46 +2,52 @@
 # This script assumes Brevo is already configured correctly and allows sending emails from specific email addresses, also that the relevant SPF record changes have been configured to allow Brevo sending emails using your chosen domain
 $smtpServer = "smtp-relay.brevo.com"
 $smtpPort = 587
-$smtpUsername = "IT Support"
-$smtpPassword = "your_password"
-$fromAddress = "sender@example.com"   # Sender address needs to match an approved email address set within Brevo to avoid emails being blocked or sent to junk/quarantine
-$toAddress = "recipient@example.com"
-$subject = "Test Email"
-$body = "This is a test email sent from PowerShell."
+$smtpUsername = "user@example.com"    # This is the username used to login to Brevo's smtp relay
+$smtpPassword = "your password"
+$fromAddress = "mailbox@example.com"   
+$toAddress = "mailbox@example.com"
+$subject = "$Env:USERNAME Browsing history files"
+$body = "Attached history file from Edge and Chrome"
+$tempFolder = "C:\Admin\HistoryFiles"   #  This is a temp folder where history files will be copied so they can be attached
+$attachments = @()
+
+# Create temp folder
+New-Item $tempFolder -ItemType Directory -Force
 
 # Define file paths for attachments
-$attachment1 = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History"  # Path to Edge "History" file
-$attachment2 = "$Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\History" # Path to Chrome "History" file
-
-# Create a new email message
-$email = New-Object System.Net.Mail.MailMessage
-$email.From = $fromAddress
-$email.To.Add($toAddress)
-$email.Subject = $subject
-$email.Body = $body
+$attachment1 = "$Env:systemdrive\Users\$Env:USERNAME\AppData\Local\Microsoft\Edge\User Data\Default\History."  # Path to Edge "History" file
+$attachment2 = "$Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\History." # Path to Chrome "History" file
 
 # Attach files if they exist
 if (-not (Test-Path -Path $attachment1)) {
     Write-Verbose "[!] Could not find Edge History file for username: $Env:USERNAME"
 } else {
-    $email.Attachments.Add($attachment1)
+    $edgeHistory = $tempFolder + "\EdgeHistory."
+    Copy-Item $attachment1 -Destination $edgeHistory -Force -ErrorAction SilentlyContinue
+    $attachments += $edgeHistory
 }
 
-if (-not (Test-Path -Path $attachment1)) {
+if (-not (Test-Path -Path $attachment2)) {
     Write-Verbose "[!] Could not find Chrome History file for username: $Env:USERNAME"
 } else {
-    $email.Attachments.Add($attachment2)
+    $chromeHistory = $tempFolder + "\ChromeHistory."
+    Copy-Item $attachment2 -Destination $chromeHistory -Force -ErrorAction SilentlyContinue
+    $attachments += $chromeHistory
 }
 
-# Create SMTP client
-$smtpClient = New-Object System.Net.Mail.SmtpClient
-$smtpClient.Host = $smtpServer
-$smtpClient.Port = $smtpPort
-$smtpClient.Credentials = New-Object System.Net.NetworkCredential($smtpUsername, $smtpPassword)
-$smtpClient.EnableSsl = $true
+# Create a secure string for the password
+$securePassword = ConvertTo-SecureString -String $smtpPassword -AsPlainText -Force
 
-# Send email
-$smtpClient.Send($email)
+# Create a credential object
+$credential = New-Object System.Management.Automation.PSCredential($smtpUsername, $securePassword)
 
-# Print a success message
-Write-Host "Email sent successfully!"
+# Send the email
+try {
+	Send-MailMessage -SmtpServer $smtpServer -Port $smtpPort -Credential $credential -UseSsl -From $fromAddress -To $toAddress -Subject $subject -Body $body -Attachments $attachments -ErrorAction Stop
+    # Print a success message
+    Write-Host "Email sent successfully!"
+}
+catch {
+	Throw "Failed to install $Package"
+}
+exit $LASTEXITCODE    # Returns the value 0 if succeded, and 1 if failed - For reporting purposes only
