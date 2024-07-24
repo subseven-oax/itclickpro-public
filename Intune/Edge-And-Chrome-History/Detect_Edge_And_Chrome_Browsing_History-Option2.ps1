2b951157afbb09de7f0bc2f7d5e7c56e8a8f0534
Victor Valentin <victor@itclickpro.com>
2024-07-24 22:39:46 +0100

    Added option 2 for detection

---
+++ Intune/Edge-And-Chrome-History/Detect_Edge_And_Chrome_Browsing_History-Option2.ps1
@@ -0,0 +1,18 @@
+﻿# Define the file path
+
+$filePath = "$Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\History" # Path to Chrome "History" file
+# Check if Chrome history file exists
+if (Test-Path $filePath) {
+    Write-Output "Chrome history file exists."
+    Exit 1
+} else {
+    $filePath = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History"  # Path to Edge "History" file
+    # Check if Edge history file exists
+    if (Test-Path $filePath) {
+        Write-Output "Edge history file exists."
+        Exit 1
+    } else {
+        Write-Output "Neither Chrome or Edge history files exist"
+        Exit 0
+    }
+}
