d6eafbd5028f23e2a006dc4c11b0f3002248a824
Victor Valentin <victor@itclickpro.com>
2024-12-17 10:20:37 +0000

    Change to detect if user exists only

--- Intune/Create-Local-admin-plus-remediation/Detect-local-admin-password-expiry.ps1
+++ Intune/Create-Local-admin-plus-remediation/Detect-local-admin-password-expiry.ps1
@@ -1,19 +1,14 @@
 # Define the account name and password
-$accountName = "ExistingAdmin"
+$accountName = "ExistingUser"
 
 # Check if the account exists
 $account = Get-LocalUser -Name $accountName -ErrorAction SilentlyContinue
 
 if ($account) {
-    # Account exists, check if the password has expired
-    if ($account.PasswordExpires) {
-        Write-Output "The password for account '$accountName' has expired."
+        Write-Output "The account '$accountName' exists."
         Exit 1
-    } else {
-        Write-Output "The password for account '$accountName' has not expired."
-        Exit 0
-    }
 } else {
     # Account does not exist
+    Write-Output "The account '$accountName' doesn't exist."
     Exit 0
 }
