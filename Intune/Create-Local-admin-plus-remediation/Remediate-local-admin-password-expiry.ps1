e16a8d919c41403d0f24c6985ba90dc47ed22b75
Victor Valentin <victor@itclickpro.com>
2025-01-14 15:47:29 +0000

    Fixed remdiate local admin password expiry script

--- Intune/Create-Local-admin-plus-remediation/Remediate-local-admin-password-expiry.ps1
+++ Intune/Create-Local-admin-plus-remediation/Remediate-local-admin-password-expiry.ps1
@@ -6,12 +6,12 @@ $account = Get-LocalUser -Name $accountName -ErrorAction SilentlyContinue
 
 if ($account) {
     # Set account and password to never expire
-    Set-LocalUser -Name $accountName -PasswordNeverExpires $true -AccountNeverExpires $true
+    Set-LocalUser -Name $accountName -PasswordNeverExpires:$true -AccountNeverExpires:$true
     
     # Disable the option to change password at first logon
-    $user = ADSI
-    $user.PasswordExpired = $false
-    $user.SetInfo()
+    $user = [adsi]"WinNT://$env:computername/$accountName"
+    $user.UserFlags.value = $user.UserFlags.value -bor 0x10000
+    $user.CommitChanges()
 
     Write-Output "Account '$accountName' has been modified: password and account set to never expire, and password change at first logon disabled."
 } else {
